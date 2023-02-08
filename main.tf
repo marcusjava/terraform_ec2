0e10cdb453879dc647b2ed8ac962459930c90e0c
marcus <buba.sergipe@gmail.com>
2023-02-07 22:09:55 -0300

    dynamodb added

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+
+/*  
+For multi region you can use alias
+Usage with provider in resource module
+provider = "aws.us-east-2"
+
+
+*/
+/* provider "aws" {
+  alias = "us-east-2"
+  region = "us-east-2"
+} */
--- servers.tf
+++ servers.tf
@@ -10,6 +10,44 @@ resource "aws_instance" "development" {
 
 }
 
+
+resource "aws_dynamodb_table" "dynamodb-table" {
+  name         = "GameScores"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "UserId"
+  range_key    = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
+
+resource "aws_instance" "development_with_dynamodb" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_https.id, aws_security_group.allow_http.id]
+  depends_on = [
+    aws_dynamodb_table.dynamodb-table
+  ]
+  tags = {
+    Name = "development_with_dynamodb"
+  }
+
+}
+
 resource "aws_instance" "development_with_bucket" {
   ami                    = var.ami
   instance_type          = var.instance_type
